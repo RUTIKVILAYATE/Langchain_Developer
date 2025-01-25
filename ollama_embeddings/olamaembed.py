9e1f72f9a5301a89075f6a198f72341cd2b01fe5
Rutik Vilayate <105170592+RUTIKVILAYATE@users.noreply.github.com>
2025-01-25 22:07:43 +0530

    Add files via upload

---
+++ ollama_embeddings/olamaembed.py
@@ -0,0 +1,11 @@
+# Ollama -> Open Source Platform where we can able to use open source models like 
+# Llama3, Phi 3, Mistral, Gemma and other models 
+# Basically we can download ollama in local machine in which we download models and can run within it 
+
+
+
+# Download Ollama -> approx. 275 mb
+# Open command prompt -> ollama run gemma:2b -> it will download this model if not present in local machine else it will run the model directly
+# we can chat with it in the command line itself -> as it is a llm trained by google and can make conversation 
+
+
